34cf86add17a98cb4e88fa621765b8a661a9e4fa
Evan Tucker <eej.tucker@gmail.com>
2018-01-09 19:00:54 -0700

    added stuff to server

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -1,3 +1,30 @@
 $(document).ready(()=> {
+    
+// Global Variables
+// ----------------------------------------------------------------------------------------------------
     $('.modal').modal();
+
+
+// Functions
+// ----------------------------------------------------------------------------------------------------
+
+    // create new user
+    const createUser = ()=> {
+
+    }
+
+    // login
+    const userLogin = ()=> {
+        let username = $('#username').val();
+        let password = $('#password').val();
+        console.log('username: ' + username + "\npassword: " + password);
+    }
+
+
+
+// Main Process
+// ----------------------------------------------------------------------------------------------------
+
+$('#submitUser').on('click', userLogin);
+
 });
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(morgan('dev'));
+
+mongoose.connect('mongodb://localhost/login', { useMongoClient: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connected to db");
+});
 
 app.get('/', (req, res, next) => {
     res.sendFile('public/index.html');
